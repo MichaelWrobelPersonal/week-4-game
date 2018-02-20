fbbbe83420ea1332966bcdd7d69bc0b2e1e021d6
MichaelWrobelPersonal <michaelrwrobel@sbcglobal.net>
2018-02-19 23:56:40 -0500

    Basically Working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,69 +16,72 @@ $(document).ready(function() {
     //Intialize display
     updateOutputs("Click a gem to start!");
    
-    // Event handlers
-    $('#red-crystal').on('click'), function(e) {
-        console.log('red-crystal')
-        theCount += rubyCountArray[0];
-        common_processing();
-    };
+// Event handlers
+$("#red-crystal").on("click", function(event) {
+    console.log('red-crystal')
+    theCount += rubyCountArray[0];
+    common_processing();
+});
 
-    $('#blue-crystal').on('click'), function(e) {
-        console.log('blue-crystal')
-        theCount += rubyCountArray[1];
-        common_processing();
-    };
+$("#blue-crystal").on("click", function(event) {
+    console.log('blue-crystal')
+    theCount += rubyCountArray[1];
+    common_processing();
+});
 
-    $('#yellow-crystal').on('click'), function(e) {
-        console.log('yellow-crystal')
-        theCount += rubyCountArray[2];
-        common_processing();
-    };
+$("#yellow-crystal").on("click", function(event) {
+    console.log('yellow-crystal')
+    theCount += rubyCountArray[2];
+    common_processing();
+});
 
-    $('#green-crystal').on('click'), function(e) {
-        console.log('green-crystal')
-        theCount += rubyCountArray[3];
-        common_processing();
-    };
+$("#green-crystal").on("click", function(event) {
+    console.log('green-crystal')
+    theCount += rubyCountArray[3];
+    common_processing();
+});
    
-    // common game functionality 
-    function common_processing()
-    {
-        /* Check for completion/exceeding the limit */
-        if ( theCount > theNumber)
-            playerExceededNumber = true;
-        if ( theNumber == theNumber)
-            playerCompletedNumber = true;
+// common game functionality 
+function common_processing()
+{
+    /* Check for completion/exceeding the limit */
+    if ( theCount > theNumber)
+        playerExceededNumber = true;
+    if ( theCount == theNumber)
+        playerCompletedNumber = true;
 
-          if (playerCompletedNumber)
-            {
-                winCount++;
-                $('#info-text').text("Victory");
-                console.log( 'Player Won' );
-            }
-            else if (playerExceededNumber)
-            {
-                lossCount++;
-                $('#info-text').text("Defeat");
-                console.log( 'Player Lost' );
-            }
-            else
-            {
-                console.log( 'Keep guessing' );
-            }
+    console.log( "The Count: " + theCount );
+    console.log( "The Number: " + theNumber ); 
+
+    if (playerCompletedNumber)
+    {
+        winCount++;
+        $('#info-text').text("Victory");
+        console.log( 'Player Won' );
+    }
+    else if (playerExceededNumber)
+    {
+        lossCount++;
+        $('#info-text').text("Defeat");
+        console.log( 'Player Lost' );
+    }
+    else
+    {
+        console.log( 'Keep guessing' );
+    }
   
-        // Reset to a new work if player guessed all of the words and then pressed Enter  
-        if ( (playerCompletedNumber || playerExceededNumber) ) // Keycode 13 is Enter
-        {
-           // Reset the Game;
-           gameReset(); 
-           // Pick another word
-           theNumber = pickANumber();
-           $('#info-text').text("Press any key to start");                                                                           
-        }
+    // Reset to a new work if player guessed all of the words and then pressed Enter  
+    if ( (playerCompletedNumber || playerExceededNumber) ) // Keycode 13 is Enter
+    {
+        // Reset the Game;
+        gameReset(); 
+        // Pick another word
+        theNumber = pickANumber();
+        $('#info-text').text("Press any key to start");                                                                           
+    }
      
-        // Output results to the Page for the user to see
-        updateOutputs("");
+    // Output results to the Page for the user to see
+    updateOutputs("");
 };
 
 function gameReset()
@@ -101,10 +104,10 @@ function pickANumber( ) {
     var selectedNumber = randnum + 19;
     console.log('Selected Number: ' + selectedNumber);
     // Pick counts for the rubies 
-    rubyCountArray[0] = ((Math.floor(Math.random()) * randnum) + 1);
-    rubyCountArray[1] = ((Math.floor(Math.random()) * randnum) + 1);
-    rubyCountArray[2] = ((Math.floor(Math.random()) * randnum) + 1);
-    rubyCountArray[3] = ((Math.floor(Math.random()) * randnum) + 1);
+    rubyCountArray[0] = Math.floor((Math.random() * 10) + 1);
+    rubyCountArray[1] = Math.floor((Math.random() * 10) + 1);
+    rubyCountArray[2] = Math.floor((Math.random() * 10) + 1);
+    rubyCountArray[3] = Math.floor((Math.random() * 10) + 1);
     for (var i=0; i<rubyCountArray.length; i++)
     {
         console.log('Ruby ' + (i+1) + ': ' + rubyCountArray[i] );
